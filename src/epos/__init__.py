1e36303191a973b008264dc65d06b27459993554
fungone76-ux <fungone76@gmail.com>
2026-08-01 23:51:20 +0200

    wire Resort beach runtime fixes

--- src/epos/__init__.py
+++ src/epos/__init__.py
@@ -2,8 +2,10 @@
 
 from .resort_runtime_patches import install_resort_runtime_patches
 from .resort_npc_action_patch import install_resort_npc_action_patch
+from .resort_beach_runtime_patch import install_resort_beach_runtime_patch
 
 install_resort_runtime_patches()
 install_resort_npc_action_patch()
+install_resort_beach_runtime_patch()
 
 __version__ = "0.1.0"
